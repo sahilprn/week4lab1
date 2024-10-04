e1f94072badad428b7d9780af301cfb39b2c45ed
sahilpreet singh <sahilpreetsingh84884@gmail.com>
2024-10-04 14:15:23 +0300

    first commit

---
+++ intro-react-project/week4-lab1p2/src/App.jsx
@@ -0,0 +1,15 @@
+// App.jsx
+import Hello from './ Hello';
+import Bye from './Bye';
+
+// App.jsx
+function App() {
+  return (
+    <div>
+      <Hello />
+      <Bye />
+    </div>
+  );
+}
+
+export default App;
